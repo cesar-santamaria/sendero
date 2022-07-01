18f6643024db12074ae06d1a31bd2894b0c8849b
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-07-01 08:36:35 -0500

    Add redirect functionality for dashboard if user is not logged in

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
 import Navbar from '../../components/navbar/Navbar';
 import { Outlet } from 'react-router-dom';
-
-
-
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function Dashboard() {
+  const navigate = useNavigate()
+  const {user} = useSelector((state) => state.auth)
+
+  useEffect(() => {
+    if(!user){
+      navigate("/login")
+    }
+  })
 
   return (
     <>
