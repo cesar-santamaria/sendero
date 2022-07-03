98d92c55d986deb0d0fdf574194ff5edac098fa1
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-07-03 18:56:01 -0500

    Style pie chart

--- client/src/pages/stats/Stats.jsx
+++ client/src/pages/stats/Stats.jsx
@@ -1,9 +1,6 @@
-import {useEffect} from 'react'
+import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import {
-  Box,
-  Typography,
-} from '@mui/material'
+import { Box, Typography } from '@mui/material'
 
 import {
   Chart,
@@ -20,35 +17,32 @@ import {
 
 import { getJob } from '../../features/jobs/jobSlice'
 
-
-
-const jobPalette = ['#82919E', '#9CA78F', '#90BE6D', '#43AA8B', '#F94144'];
+const jobPalette = ['#245187', '#3abca2', '#d5513b', '#e38b3f', '#cf334b']
 
 export default function Stats() {
   const { jobs } = useSelector((state) => {
     return state.jobs
   })
   const dispatch = useDispatch()
-  const interestedJobs = jobs.filter((job)=> job.status === 'interested')
-  const appliedJobs = jobs.filter((job)=> job.status === 'applied')
-  const interviewJobs = jobs.filter((job)=> job.status === 'interview')
-  const offerJobs = jobs.filter((job)=> job.status === 'offer')
-  const rejectedJobs = jobs.filter((job)=> job.status === 'rejected')
+  const interestedJobs = jobs.filter((job) => job.status === 'interested')
+  const appliedJobs = jobs.filter((job) => job.status === 'applied')
+  const interviewJobs = jobs.filter((job) => job.status === 'interview')
+  const offerJobs = jobs.filter((job) => job.status === 'offer')
+  const rejectedJobs = jobs.filter((job) => job.status === 'rejected')
 
   const chartData = [
-    { status: "Interested", jobs: interestedJobs.length },
-    { status: "appliedJobs", jobs: appliedJobs.length },
-    { status: "interviewJobs", jobs: interviewJobs.length },
-    { status: "offerJobs", jobs: offerJobs.length },
-    { status: "rejectedJobs", jobs: rejectedJobs.length },
+    { status: 'Interested', jobs: interestedJobs.length },
+    { status: 'Applied', jobs: appliedJobs.length },
+    { status: 'Interview', jobs: interviewJobs.length },
+    { status: 'Offer', jobs: offerJobs.length },
+    { status: 'Rejected', jobs: rejectedJobs.length },
   ]
 
   console.log(rejectedJobs.length)
 
-  useEffect(()=> {
+  useEffect(() => {
     dispatch(getJob())
-  },[dispatch])
-
+  }, [dispatch])
 
   return (
     <section
@@ -72,10 +66,10 @@ export default function Stats() {
           </Typography>
         </Box>
         <Chart data={chartData}>
-        <Palette scheme={jobPalette} />
+          <Palette scheme={jobPalette} />
           <PieSeries valueField="jobs" argumentField="status"></PieSeries>
           <Animation />
-          <Legend/>
+          <Legend />
           <EventTracker />
           <HoverState />
           <Tooltip />
