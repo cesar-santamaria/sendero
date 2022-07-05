6f879197e861fa52f1240809e0cecd8be8d50282
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-07-05 12:49:13 -0500

    Refactor path in server.js

--- server.js
+++ server.js
@@ -31,9 +31,9 @@ app.use('/api/jobs', jobRoutes);
 
 //server frontend
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname,'../client/build')))
+  app.use(express.static(path.join(__dirname,'./client/build')))
 
-  app.get('*',(req,res) => res.sendFile(path.resolve(__dirname, '../','client','build','index.html')))
+  app.get('*',(req,res) => res.sendFile(path.resolve(__dirname, './','client','build','index.html')))
 } else {
   app.get('/', (req,res) => res.send("Please node_env to production"))
 }
